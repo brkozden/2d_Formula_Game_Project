c0e9be7a2ba3a8ff81f981f4dd7f97639e9c81f5
berko <berko@DESKTOP-DB18UJA>
2022-07-22 23:02:19 +0300

    Add project files.

---
+++ 2d_Formula_Game_Project/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _2d_Formula_Game_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.line1 = new System.Windows.Forms.Label();
+            this.line2 = new System.Windows.Forms.Label();
+            this.line3 = new System.Windows.Forms.Label();
+            this.line4 = new System.Windows.Forms.Label();
+            this.line5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.coins = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coins)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // line1
+            // 
+            this.line1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.line1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.line1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line1.Location = new System.Drawing.Point(310, 6);
+            this.line1.Name = "line1";
+            this.line1.Size = new System.Drawing.Size(25, 150);
+            this.line1.TabIndex = 0;
+            // 
+            // line2
+            // 
+            this.line2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.line2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.line2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line2.Location = new System.Drawing.Point(310, 306);
+            this.line2.Name = "line2";
+            this.line2.Size = new System.Drawing.Size(25, 150);
+            this.line2.TabIndex = 1;
+            // 
+            // line3
+            // 
+            this.line3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.line3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.line3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line3.Location = new System.Drawing.Point(310, 600);
+            this.line3.Name = "line3";
+            this.line3.Size = new System.Drawing.Size(25, 150);
+            this.line3.TabIndex = 2;
+            // 
+            // line4
+            // 
+            this.line4.BackColor = System.Drawing.Color.Yellow;
+            this.line4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.line4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line4.Location = new System.Drawing.Point(0, 0);
+            this.line4.Name = "line4";
+            this.line4.Size = new System.Drawing.Size(25, 857);
+            this.line4.TabIndex = 3;
+            // 
+            // line5
+            // 
+            this.line5.BackColor = System.Drawing.Color.Yellow;
+            this.line5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.line5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line5.Location = new System.Drawing.Point(625, 0);
+            this.line5.Name = "line5";
+            this.line5.Size = new System.Drawing.Size(25, 857);
+            this.line5.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblScore
+            // 
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblScore.Location = new System.Drawing.Point(519, 5);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(100, 28);
+            this.lblScore.TabIndex = 7;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // player2
+            // 
+            this.player2.Image = global::_2d_Formula_Game_Project.Properties.Resources.black;
+            this.player2.Location = new System.Drawing.Point(98, 241);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(125, 215);
+            this.player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player2.TabIndex = 6;
+            this.player2.TabStop = false;
+            // 
+            // player1
+            // 
+            this.player1.Image = global::_2d_Formula_Game_Project.Properties.Resources.black;
+            this.player1.Location = new System.Drawing.Point(442, 373);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(125, 215);
+            this.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player1.TabIndex = 5;
+            this.player1.TabStop = false;
+            // 
+            // coins
+            // 
+            this.coins.Image = global::_2d_Formula_Game_Project.Properties.Resources.Gold_Coin_PNG_Clipart_663;
+            this.coins.Location = new System.Drawing.Point(177, 539);
+            this.coins.Name = "coins";
+            this.coins.Size = new System.Drawing.Size(57, 49);
+            this.coins.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coins.TabIndex = 8;
+            this.coins.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(650, 600);
+            this.Controls.Add(this.coins);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.line5);
+            this.Controls.Add(this.line4);
+            this.Controls.Add(this.line3);
+            this.Controls.Add(this.line2);
+            this.Controls.Add(this.line1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coins)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label line1;
+        private System.Windows.Forms.Label line2;
+        private System.Windows.Forms.Label line3;
+        private System.Windows.Forms.Label line4;
+        private System.Windows.Forms.Label line5;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox coins;
+    }
+}
+
---
+++ 2d_Formula_Game_Project/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2d_Formula_Game_Project
+{
+    
+    public partial class Form1 : Form
+    {
+        int line1y, line2y, line3y,lineX,coinx,coiny,coin_speed = 5;
+        int line_speed = 20,car_speed=8,score=0;
+        
+        int player2x, player2y,turnValue=25;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right && player1.Location.X<=350)
+            {
+                player1.Left+= turnValue;
+             
+           
+            }
+         
+            if (e.KeyCode == Keys.Left && player1.Location.X >= 35)
+            {
+                player1.Left -= turnValue ;
+
+            }
+        }
+
+        Random random = new Random();
+        Image[] images = 
+        {
+            Properties.Resources.boom,
+            Properties.Resources.black,
+            Properties.Resources.white,
+            Properties.Resources.blue,
+
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Start();
+           
+          
+        }
+        void Start()
+        {
+            score = 0;
+            player1.Image = images[random.Next(1, 4)];
+            player2.Image = images[random.Next(1, 4)];
+
+            timer1.Enabled = true;
+            timer1.Interval = 5;
+            line1y = line1.Location.Y;
+            line2y = line2.Location.Y;
+            line3y = line3.Location.Y;
+            lineX = line1.Location.X;
+
+
+            player2y = random.Next(-3000, -100);
+            player2x = random.Next(25, 370);
+            player2.Location = new Point(player2x, player2y);
+            lblScore.Text = "Score: " + score;
+
+            coinx = random.Next(30, 560);
+            coiny = random.Next(115, 540);
+            coins.Location = new Point(coinx, coiny);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            line1y+=line_speed;
+            line2y+= line_speed;
+            line3y+= line_speed;
+            line1.Location = new Point(lineX,line1y);
+            line2.Location = new Point(lineX, line2y);
+            line3.Location = new Point(lineX, line3y);
+
+            if (line3y>=600)
+            {
+                line3y=-150;
+
+            }
+            if (line2y >= 600)
+            {
+                line2y = -150;
+
+            }
+            if (line1y >= 600)
+            {
+                line1y = -150;
+
+            }
+
+            player2y += car_speed; 
+            player2.Location = new Point(player2x, player2y);
+            coiny += coin_speed;
+            coins.Location=new Point(coinx, coiny);
+
+            if (coiny>540)
+            {
+                coiny = random.Next(-500, -215);
+                coinx = random.Next(25, 370);
+                
+            }
+
+            if (player2y > 670)
+            {
+                player2y = random.Next(-500, -215);
+                player2x = random.Next(25, 370);
+                player2.Image = images[random.Next(1,4)];
+                 car_speed = random.Next(2, 10);
+                
+               
+
+            }
+
+            if (player1.Bounds.IntersectsWith(player2.Bounds))
+            {  
+                player1.Image = images[0];
+                player2.Image = images[0];
+                timer1.Enabled = false;
+                string text1 = "Game Over \n" + lblScore.Text;
+                DialogResult dialog = MessageBox.Show(text1, "Game Over",MessageBoxButtons.RetryCancel);
+                if (dialog == DialogResult.Retry)
+                {
+                    Start();
+                }
+                if (dialog == DialogResult.Cancel)
+                {
+                    Application.Exit();
+                }
+
+
+
+            }
+            if (player1.Bounds.IntersectsWith(coins.Bounds) )
+            {
+                score++;
+                coiny = random.Next(-500, -215);
+                coinx = random.Next(25, 370);
+                lblScore.Text = "Score: " + score;
+            }
+       
+
+        }
+    }
+}
